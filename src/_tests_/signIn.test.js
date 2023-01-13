fc6f4bf66f0efa3bf3c4f02e88d1187d5caafcdd
webor <tambatvibhor@gmail.com>
2023-01-13 11:25:00 +0530

    sanitised tests

--- src/_tests_/signIn.test.js
+++ src/_tests_/signIn.test.js
@@ -22,42 +22,40 @@ describe('signin tests', () => {
         return { username, password, submitBtn };
     }
 
-    it('displays ui correctly', () => {
-        const handleSubmit = jest.fn();
-        const { view } = render(<SignIn onSubmit={handleSubmit} />);
-        const username = screen.getByRole('textbox', { name: /username/i });
-        const password = screen.getByLabelText(/password/i);
-        const button = screen.getByRole('button', { name: /submit/i });
+    it('displays all form elements', () => {
+        const { username, password, submitBtn } = initializeLogin();
+        expect(username).toBeInTheDocument();
+        expect(password).toBeInTheDocument();
+        expect(submitBtn).toBeInTheDocument();
     });
 
     it('username/password cannot be empty', () => {
         const { username, password, submitBtn, handleSubmit } = initializeLogin();
-        userEvent.type(username, 'pushpa');
-        userEvent.type(password, 'fire hai');
+        userEvent.type(username, 'john');
+        userEvent.type(password, 'Abc@123');
         userEvent.click(submitBtn);
         expect(handleSubmit).toHaveBeenCalledTimes(1);
-        expect(username).toHaveValue('pushpa');
-        expect(password).toHaveValue('fire hai');
+        expect(username).toHaveValue('john');
+        expect(password).toHaveValue('Abc@123');
     });
 
     it('btn shows loader when clicked', async () => {
         const { username, password, submitBtn } = initializeApp();
-        userEvent.type(username, 'pushpa');
-        userEvent.type(password, 'fire hai');
+        userEvent.type(username, 'john');
+        userEvent.type(password, 'Abc@123');
         userEvent.click(submitBtn);
         await waitFor(() => {
             screen.getByAltText('loader');
         })
         await waitForElementToBeRemoved(screen.queryByAltText('loader'), { timeout: 5000 }).then(() => {
-            screen.debug();
-            expect(screen.getByText(/pushpa/i)).toBeInTheDocument();
+            expect(screen.getByText(/john/i)).toBeInTheDocument();
         });
     });
 
     it('invalid signin credentials case', async () => {
         const { username, password, submitBtn } = initializeApp();
-        userEvent.type(username, 'pushpa');
-        userEvent.type(password, 'flower hai');
+        userEvent.type(username, 'john');
+        userEvent.type(password, 'qwerty');
         userEvent.click(submitBtn);
         await waitFor(() => {
             expect(screen.queryAllByText(/Either username or password incorrect/i))
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,7 +2,7 @@ import { rest } from 'msw'
 export const handlers = [
     rest.post('https://test_demo/api/login', (req, res, ctx) => {
         const { username, password } = req.body;
-        if (username == 'pushpa' && password == 'fire hai') {
+        if (username == 'john' && password == 'Abc@123') {
             return res(
                 ctx.delay(2000),
                 ctx.json({ username })
